59a4436403d04dcd68abe0bcd79491adf34875f9
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-07-12 22:34:14 -0400

    declairing cost

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+(() => {
+  const name = document.querySelector(".teamInfo h2"); // name
+  const contents = document.querySelector(".detail"); // members detail
+  const position = document.querySelector(".teamInfo h3"); //members position
+  const buttons = document.querySelectorAll(".bioSwitcher button"); //buttons
+  const skillsIcon = document.querySelector("#skillsIcon"); //icons
+  const des = document.querySelector("#profileJer"); //profile 1
+  const dev = document.querySelector("#profilePwn"); //profile 2
+})();
